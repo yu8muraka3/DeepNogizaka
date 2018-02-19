42aed856e3d5dc70587034b9422cc0bfa732932e
yuya <yuyatennish@gmail.com>
2018-02-20 07:09:44 +0900

    flask

---
+++ NogizakaApp/app.py
@@ -0,0 +1,60 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from werkzeug import secure_filename
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "Hello World"
+    return render_template('index.html', title='flask test', name=name)
+
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
+
+###########################
+#以下画像アップロード処理
+##########################
+
+#iPhoneの画像ファイル形式への対応
+
+#画像アップロード
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = os.urandom(24)
+
+#アップロードを許可する画像ファイル
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+#画像POST処理
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        img_file = request.files['img_file']
+        if img_file and allowed_file(img_file.filename):
+            filename = secure_filename(img_file.filename)
+            img_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            img_url = '/uploads/' + filename
+            return render_template('index.html', img_url=img_url)
+        else:
+            return ''' <p>許可されていない拡張子です</p> '''
+    else:
+        return redirect(url_for('index'))
+
+
+#アップロードされた画像ファイル
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
