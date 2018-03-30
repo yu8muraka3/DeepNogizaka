53b48a59556efd816e9cafab725fcd3e7ed2d48e
yuya <yuyatennish@gmail.com>
2018-03-30 11:14:45 +0900

    Organize

--- NogizakaApp/app.py
+++ NogizakaApp/app.py
@@ -1,6 +1,7 @@
-import os
+import os, re
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
 from werkzeug import secure_filename
+from detect import start_detect
 
 
 app = Flask(__name__)
@@ -23,12 +24,14 @@ def good():
 
 #画像アップロード
 UPLOAD_FOLDER = './static/asset/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+ALLOWED_EXTENSIONS = set(['jpg'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = os.urandom(24)
 
 #アップロードを許可する画像ファイル
 def allowed_file(filename):
+    # ファイル名の拡張子より前を取得し, フォーマット後のファイル名に変更
+    filename = re.search("(?<!\.)\w+", filename).group(0) + "." + 'jpg'
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
@@ -42,7 +45,21 @@ def send():
             filename = secure_filename(img_file.filename)
             img_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             img_url = '/uploads/' + filename
-            return render_template('index.html', img_url=img_url)
+            predict_name, predict_enname, rate = start_detect(filename)
+
+            if predict_name != None:
+                if predict_enname != "Akimoto Yasushi":
+                    predict_enname = predict_enname.lower()
+                    enname = predict_enname.split()
+                    url_enname = enname[1] + "." + enname[0]
+                else:
+                    url_enname = None
+
+                print(predict_name)
+                # return render_template('index.html', img_url=img_url)
+                return render_template('result.html', name=predict_name, urlname=url_enname, rate=rate, filename=filename)
+            else:
+                return render_template('index.html', error="error")
         else:
             return ''' <p>許可されていない拡張子です</p> '''
     else:
---
+++ NogizakaApp/detect.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from keras.models import load_model
+from flask import request
+from operator import itemgetter
+
+categories = ["asuka", "ikoma", "ikuta", "maiyan", "nanase", "yasushi"]
+
+
+def detect_face(image):
+    print(image.shape)
+    #opencvを使って顔抽出
+    # image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cascade = cv2.CascadeClassifier("/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml")
+    # 顔認識の実行
+    face_list=cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=1,minSize=(64,64))
+    #顔が１つ以上検出された時
+    if len(face_list) > 0:
+        face_info = [[]]
+        for rect in face_list:
+            x = rect[0]
+            y = rect[1]
+            width = rect[2]
+            height = rect[3]
+            face_size = width * height
+            img = image[y:y + height, x:x + width]
+            face_info.append([face_size, img, rect[0:2], rect[2:4], x, y, height])
+            # x,y,width,height=rect
+
+            # img = image[rect[1]:rect[1]+rect[3],rect[0]:rect[0]+rect[2]]
+            if image.shape[0]<64:
+                print("too small")
+                continue
+
+        face_info.sort(reverse = True)
+        predict_img = face_info[0][1]
+        print(face_info[0][2:4])
+        cv2.rectangle(image, tuple(face_info[0][2]), tuple(face_info[0][2]+face_info[0][3]), (255, 0, 0), thickness=3)
+        img = cv2.resize(predict_img,(64, 64))
+        img = np.expand_dims(img,axis=0)
+        predict_name, predict_enname, rate = detect_who(img)
+        show = cv2.putText(image,predict_enname,(face_info[0][4]-20,face_info[0][5]+face_info[0][6]+80),cv2.FONT_HERSHEY_DUPLEX,3,(255,0,0),2)
+        cv2.imwrite("./static/asset/uploads/" + img_file, show[:, :, ::-1].copy())
+
+        return predict_name, predict_enname, rate
+    #顔が検出されなかった時
+    else:
+        print("no face")
+        return None, None
+
+def detect_who(img):
+    #予測
+    name=""
+    model = load_model('./my_model.h5')
+
+    # nameNumLabel=np.argmax(model.predict(img))
+    predict = model.predict_proba(img)
+    for i, pre in enumerate(predict):
+        idx = np.argmax(pre)
+        rate = pre[idx] * 100
+        rate = str(round(rate, 1))
+        if idx == 0:
+            name="齋藤飛鳥"
+            en_name="Saito Asuka"
+        elif idx == 1:
+            name="生駒里奈"
+            en_name="Ikoma Rina"
+        elif idx == 2:
+            name="生田絵梨花"
+            en_name="Ikuta Erika"
+        elif idx == 3:
+            name="白石麻衣"
+            en_name="Shiraishi Mai"
+        elif idx == 4:
+            name="西野七瀬"
+            en_name="Nishino Nanase"
+        elif idx == 5:
+            name="秋元康"
+            en_name="Akimoto Yasushi"
+
+    return name, en_name, rate
+
+def start_detect(input_file):
+    global img_file
+    img_file = input_file
+    model = load_model('./my_model.h5')
+
+    image=cv2.imread("./static/asset/uploads/" + img_file)
+    print(img_file)
+    if image is None:
+        print("Not open:")
+    b,g,r = cv2.split(image)
+    image = cv2.merge([r,g,b])
+    predict_name, predict_enname, rate = detect_face(image)
+    # whoImage=detect_face(image)
+    #
+    # plt.imshow(whoImage)
+    # plt.show()
+
+    return predict_name, predict_enname, rate
